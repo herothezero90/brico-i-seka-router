bf16c66154f6a2fe8a1e07c95164c6eee4d647bf
herothezero90 <karl.a.lenard@gmail.co>
2024-06-04 15:33:17 +0200

    changed footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Flex, Link, Icon, Text, Button, useMediaQuery } from '@chakra-ui/react';
+import { Box, Flex, Link, Icon, Text, useMediaQuery } from '@chakra-ui/react';
 import { FaPhone, FaEnvelope, FaMapMarkerAlt, FaInstagram, FaFacebook } from 'react-icons/fa';
 
 const Footer = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
   const [isMobile] = useMediaQuery("(max-width: 767px)");
   const [isShrunk, setIsShrunk] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
-
-  const handleExpandClick = () => {
-    setIsExpanded(!isExpanded);
-  };
 
   useEffect(() => {
     const controlFooter = () => {
@@ -38,7 +33,7 @@ const Footer = () => {
     <Box
       as="footer"
       bg="teal.900"
-      color="yellow.400"
+      color="yellow.500"
       py={isShrunk ? 2 : 4}
       position="fixed"
       bottom={0}
@@ -53,37 +48,31 @@ const Footer = () => {
         alignItems="center"
         flexWrap="wrap"
         px={4}
-        display={isMobile && !isExpanded ? 'none' : 'flex'}
+        display={isMobile ? 'none' : 'flex'}
       >
         <Box
           flexBasis={{ base: '100%', md: 'auto' }}
           textAlign={{ base: 'center', md: 'left' }}
           mb={{ base: 4, md: 0 }}
           fontFamily="Roper"
-          display={{ base: isExpanded ? 'block' : 'none', md: 'block' }}
         >
           <Text
             fontSize='md'
-            color="yellow.400"
             fontFamily="Roper"
-            display={{ base: "none", md: "inline" }}>
+          >
             Kontakt
           </Text>
           <Text
             fontSize='md'
-            color="yellow.400"
             fontFamily="Roper">
             Ul. kralja Zvonimira 26, Velika Gorica
           </Text>
           <Link
             href="tel:+385917886920"
             aria-label="Call barbershop on the phone"
-            color="yellow.400"
-            fontSize='md'
             fontFamily="Roper"
           >
             <Icon
-              color="yellow.400"
               as={FaPhone} mr={2}
             />
             +385 91 788 6920
@@ -92,8 +81,6 @@ const Footer = () => {
           <Link
             href="mailto:tomnorsic@gmail.com"
             aria-label="Email barbershop"
-            fontSize='md'
-            color="yellow.400"
             fontFamily="Roper"
           >
             <Icon as={FaEnvelope} mr={2} />
@@ -111,7 +98,6 @@ const Footer = () => {
             aria-label="Locate barbershop on google maps"
             mr={4}
             fontSize='md'
-            color="yellow.400"
             fontFamily="Roper"
           >
             <Icon as={FaMapMarkerAlt} mr={2} />
@@ -123,7 +109,6 @@ const Footer = () => {
               aria-label="Barbershop Instagram page"
               mr={4}
               fontSize='md'
-              color="yellow.400"
               fontFamily="Roper"
             >
               <Icon as={FaInstagram} mr={2} />
@@ -133,7 +118,6 @@ const Footer = () => {
               href="https://www.facebook.com/BricoiSeka/"
               aria-label="Barbershop Facebook page"
               fontSize='md'
-              color="yellow.400"
               fontFamily="Roper"
             >
               <Icon as={FaFacebook} mr={2} />
@@ -144,37 +128,43 @@ const Footer = () => {
       </Flex>
 
       {/* Mobile size */}
-      <Box display={isMobile ? 'flex' : 'none'} justifyContent="center" mt={2}>
-        <Button
-          size="sm"
-          mb={"2"}
-          bg='yellow.500'
-          onClick={handleExpandClick}
-        >
-          {isExpanded ? 'Manje' : 'Više'}
-        </Button>
-      </Box>
-
       <Flex
         justifyContent="space-evenly"
         alignItems="center"
-        display={isMobile && !isExpanded ? 'flex' : 'none'}
+        display={isMobile ? 'flex' : 'none'}
         mt={2}
+        flexWrap="wrap"
+        px={4}
       >
         <Link href="tel:+385917886920" aria-label="Call barbershop">
-          <Icon as={FaPhone} boxSize={6} color="yellow.500" />
+          <Flex flexDirection="column" alignItems="center">
+            <Icon as={FaPhone} boxSize={6} color="yellow.500" />
+            <Text fontSize="xs" color="yellow.500" mt={1} fontFamily="Roper">Poziv</Text>
+          </Flex>
         </Link>
         <Link href="mailto:tomnorsic@gmail.com" aria-label="Email barbershop">
-          <Icon as={FaEnvelope} boxSize={6} color="yellow.500" />
+          <Flex flexDirection="column" alignItems="center">
+            <Icon as={FaEnvelope} boxSize={6} color="yellow.500" />
+            <Text fontSize="xs" color="yellow.500" mt={1} fontFamily="Roper">Mail</Text>
+          </Flex>
         </Link>
         <Link href="https://maps.app.goo.gl/HqGnNuu93wkYoiTo7" aria-label="Locate barbershop">
-          <Icon as={FaMapMarkerAlt} boxSize={6} color="yellow.500" />
+          <Flex flexDirection="column" alignItems="center">
+            <Icon as={FaMapMarkerAlt} boxSize={6} color="yellow.500" />
+            <Text fontSize="xs" color="yellow.500" mt={1} fontFamily="Roper">Mapa</Text>
+          </Flex>
         </Link>
         <Link href="https://www.instagram.com/brico_i_seka/" aria-label="Barbershop Instagram page">
-          <Icon as={FaInstagram} boxSize={6} color="yellow.500" />
+          <Flex flexDirection="column" alignItems="center">
+            <Icon as={FaInstagram} boxSize={6} color="yellow.500" />
+            <Text fontSize="xs" color="yellow.500" mt={1} fontFamily="Roper">IG</Text>
+          </Flex>
         </Link>
         <Link href="https://www.facebook.com/BricoiSeka/" aria-label="Barbershop Facebook page">
-          <Icon as={FaFacebook} boxSize={6} color="yellow.500" />
+          <Flex flexDirection="column" alignItems="center">
+            <Icon as={FaFacebook} boxSize={6} color="yellow.500" />
+            <Text fontSize="xs" color="yellow.500" mt={1} fontFamily="Roper">FB</Text>
+          </Flex>
         </Link>
       </Flex>
     </Box>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Box, Image, Heading } from '@chakra-ui/react';
 import './Home.css';
 
 const Home = () => {
-  // Disable scrolling on mount
+
   React.useEffect(() => {
     document.body.style.overflow = 'hidden';
     return () => {
@@ -16,23 +16,25 @@ const Home = () => {
     <Box 
       className="home-container" 
       mt={0}
-      height="100vh" 
+      height={{ base: '80vh', md: '100vh' }} // Set height to 80vh on mobile screens and 100vh on larger screens
       overflow="hidden" 
       position="relative"
     >
       <Image 
         src='Images/pjenaipivo.png' 
         alt="Brico main image"
-        position="fixed"
+        position="absolute"
         top={0}
         left={0}
         width="100%"
         height="100%"
         objectFit="cover"
-        zIndex={-1} 
+        zIndex={-1}
+        transform={{ base: 'scale(1)', md: 'none' }} 
       />
 
-      <Box className="text-container"
+      <Box 
+        className="text-container"
         position="absolute"
         top="50%"
         left="50%"
@@ -40,12 +42,15 @@ const Home = () => {
         p={8}
         borderRadius={8}
         zIndex={1}
-        textAlign="center" // Center align the text
+        textAlign="center" 
       >
-        <Heading size="4xl" mb={4} 
-        fontFamily="Roper"
-        color="teal.600"
-        textAlign="center" >
+        <Heading 
+          size="4xl" 
+          mb={4} 
+          fontFamily="Roper"
+          color="teal.600"
+          textAlign="center"
+        >
           Brico i Seka
         </Heading>
         <Counter />
